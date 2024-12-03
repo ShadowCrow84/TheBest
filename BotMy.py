9e4d47c2fef3c7354bd51aed71434d172c560fca
ShadowCrow84 <minnahmetovazamat@gmail.com>
2024-12-03 14:18:02 +0000

    Duck

--- BotMy.py
+++ BotMy.py
@@ -1,4 +1,7 @@
 import telebot 
+import os
+import random
+import requests
 from telebot import types
     
     # Инициализация бота с использованием его токена
@@ -42,4 +45,24 @@ def create_poll(message):
         is_anonymous=False,
     )
     # Запуск бота
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+    
+    
+@bot.message_handler(commands=['duck'])
+def duck(message):
+        '''По команде duck вызывает функцию get_duck_image_url и отправляет URL изображения утки'''
+        image_url = get_duck_image_url()
+        bot.reply_to(message, image_url)
+
 bot.polling()
